64800381edcfd8f687d03f0b08315ef67ef18f20
barkotenicholas <barkotenicholas@gmail.com>
2022-04-02 21:12:01 +0300

    Added apiclient

--- app/src/main/java/com/example/application/network/instance/RetrofitInstance.java
+++ app/src/main/java/com/example/application/network/instance/RetrofitInstance.java
@@ -1,4 +1,24 @@
 package com.example.application.network.instance;
 
+import com.example.application.models.pojos.Post;
+import com.example.application.network.interfaces.PostCalls;
+
+import retrofit2.Retrofit;
+
 public class RetrofitInstance {
+    private static final String BASE_URL ="http://chergeionline.com:8000";
+    private static Retrofit retrofit = null;
+
+    public static PostCalls getApiClient(){
+
+        if(retrofit ==null){
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(PostCalls.class);
+    }
 }
--- app/src/main/java/com/example/application/network/interfaces/PostCalls.java
+++ app/src/main/java/com/example/application/network/interfaces/PostCalls.java
@@ -1,11 +1,16 @@
 package com.example.application.network.interfaces;
 
+import com.example.application.models.pojos.Post;
+
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface PostCalls {
 
     @GET("posts")
-
+    Call<List<Post>> getPost();
 
 
 }
