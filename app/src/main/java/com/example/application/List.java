4cfc9eb2b8d3ede0632d66ab59e506e4c4464f4b
barkotenicholas <barkotenicholas@gmail.com>
2022-04-02 21:27:58 +0300

    Added activity mainbinding on list activity

--- app/src/main/java/com/example/application/List.java
+++ app/src/main/java/com/example/application/List.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.application.databinding.ActivityListBinding;
+
 public class List extends AppCompatActivity {
+
+    private ActivityListBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        binding = ActivityListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
