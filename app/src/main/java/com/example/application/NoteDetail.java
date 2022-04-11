5a728fd1bb46cc4ef340815d4a858e4f491cfe39
barkotenicholas <barkotenicholas@gmail.com>
2022-04-11 12:41:30 +0300

    Added writing to database

--- app/src/main/java/com/example/application/NoteDetail.java
+++ app/src/main/java/com/example/application/NoteDetail.java
@@ -1,5 +1,6 @@
 package com.example.application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,6 +15,11 @@ import com.example.application.databinding.ActivityNoteDetailBinding;
 import com.example.application.models.Data;
 import com.example.application.models.pojos.CourseInfo;
 import com.example.application.models.pojos.Notes;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,6 +31,14 @@ public class NoteDetail extends AppCompatActivity {
     ActivityNoteDetailBinding binding;
     Data data;
     List<CourseInfo> list = new ArrayList<>();
+
+    // creating a variable for our
+    // Firebase Database.
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our Database
+    // Reference for Firebase.
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +48,11 @@ public class NoteDetail extends AppCompatActivity {
          data = Data.getInstance();
         Intent intent = getIntent();
         int index = intent.getIntExtra("index",0);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // below line is used to get reference for our database.
+        databaseReference = firebaseDatabase.getReference("Notes");
 
         if(index >0) {
             populate(index);
@@ -60,7 +79,23 @@ public class NoteDetail extends AppCompatActivity {
                 String content = binding.content.getText().toString().trim();
                 String course = binding.spinner.getSelectedItem().toString();
 
+
+
                 Notes noteDetail = new Notes(course,title,content);
+
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        databaseReference.setValue(noteDetail);
+                        Toast.makeText(NoteDetail.this, "data added", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
                 data.notesArrayList.add(noteDetail);
 
